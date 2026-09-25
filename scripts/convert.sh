b45caa27ca57192a64d44924f27183e1866a5253
iiAmPanda <jamesx1010@gmail.com>
2026-09-24 23:41:10 -0700

    Add files via upload

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -5,8 +5,8 @@ for file in $(find ../src/main/java -type f -name "*.java"); do
 		-e 's/@OnlyIn/@Environment/g' \
 		-e 's/Dist\.CLIENT/EnvType\.CLIENT/g' \
 		-e 's/EnvType\.DEDICATED_SERVER/EnvType\.SERVER/g' \
-		-e 's/import net\.minecraftforge\.api\.distmarker\.OnlyIn;/import net\.fabricmc\.api\.Environment;/g' \
-		-e 's/import net\.minecraftforge\.api\.distmarker\.Dist;/import net\.fabricmc\.api\.EnvType;/g' \
+		-e 's/import net\.neoforged\.api\.distmarker\.OnlyIn;/import net\.fabricmc\.api\.Environment;/g' \
+		-e 's/import net\.neoforged\.api\.distmarker\.Dist;/import net\.fabricmc\.api\.EnvType;/g' \
 	$file
 
 	echo "Converted $file"
